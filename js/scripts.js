eeed132e781fe1df0712e2ecb00e48ef4475a57b
Himanshu <hymn.zwarrior@gmail.com>
2021-01-25 21:15:41 +0530

    v0.0.0.1

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+let transitioning = false;
+
+window.onload = function() {
+    Particles.init({
+        maxParicles: 3,
+        sizeVariations: 3,
+        selector: '.background',
+        color: ['#888888'],
+        speed: 0.25,
+        connectParticles: true
+    });
+};
+
+$('body:not(.transitioning)').on('mousewheel',function(e){
+    let cS = $('section:visible'),
+        nS = $(cS).next('section'),
+        pS = $(cS).prev('section');
+
+    if( e.originalEvent.deltaY > 0 && nS.length ){
+        $('body').addClass('transitioning')
+        $(cS).fadeOut(750,function(){
+            $(nS).fadeIn(750,function(){
+                $('body').removeClass('transitioning')
+            })
+        })
+    }
+    if( e.originalEvent.deltaY < 0 && pS.length ){
+        $('body').addClass('transitioning')
+        $(cS).fadeOut(750,function(){
+            $(pS).fadeIn(750,function(){
+                $('body').removeClass('transitioning')
+            })
+        })
+    }
+});
