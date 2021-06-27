1c07958160ec467b8936fde7dc8236d09239aa0a
Himanshu Reddy <hymn.zwarrior@gmail.com>
2021-06-27 21:44:02 +0530

    change in layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let transitioning = false;
 
-$('body:not(.transitioning)').on('mousewheel',function(e){
+/* $('body:not(.transitioning)').on('mousewheel',function(e){
     let cS = $('section:visible'),
         nS = $(cS).next('section'),
         pS = $(cS).prev('section');
@@ -21,7 +21,7 @@ $('body:not(.transitioning)').on('mousewheel',function(e){
             })
         })
     }
-});
+}); */
 
 (function(){
     Particles.init({
@@ -41,3 +41,22 @@ $('body:not(.transitioning)').on('mousewheel',function(e){
         }]
     })
 })();
+
+(function($) {
+
+    $.fn.visible = function(partial) {
+      
+        var $t            = $(this),
+            $w            = $(window),
+            viewTop       = $w.scrollTop(),
+            viewBottom    = viewTop + $w.height(),
+            _top          = $t.offset().top,
+            _bottom       = _top + $t.height(),
+            compareTop    = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+      
+      return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+  
+    };
+      
+  })(jQuery);
